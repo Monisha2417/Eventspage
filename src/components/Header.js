224ba9f44e0c3118f4a89b491231fb84d9e00caa
Monisha2417 <mon24isha@gmail.com>
2024-08-22 00:40:05 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-overlay">
+        <div className="header-content">
+          <h1 className="header-heading">Our Events</h1>
+          <p className="header-tagline">Where Ideas Drive Progress</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
